61d1adf3a47ee8bf8074ae192dc4512bdbaf7051
dongyoonkim <rlark1224@naver.com>
2024-08-22 18:41:52 +0900

    update glm

--- ObjViewer/include/pch.hpp
+++ ObjViewer/include/pch.hpp
@@ -26,6 +26,7 @@
 
 // glm
 #if GLM_USE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
--- SkeletonHierarchy/include/pch.hpp
+++ SkeletonHierarchy/include/pch.hpp
@@ -30,6 +30,7 @@
 
 // glm
 #if GLM_USE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
